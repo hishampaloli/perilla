21b233ef98f9ab058e14b77f6d7403176969f7a0
Hisham Paloli <hishampaloli786@gmail.com>
2023-01-21 17:27:48 +0530

    adminDash

---
+++ client/secrets/firebase_cofigs.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6w_hgrJpjhyIXr4TdhUMLm4kpaTLwqtQ",
+  authDomain: "hr-managment-bd7dd.firebaseapp.com",
+  projectId: "hr-managment-bd7dd",
+  storageBucket: "hr-managment-bd7dd.appspot.com",
+  messagingSenderId: "11848927028",
+  appId: "1:11848927028:web:a350f44d89dc9d008dc761",
+  measurementId: "G-QVEQYQ5Y67",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
