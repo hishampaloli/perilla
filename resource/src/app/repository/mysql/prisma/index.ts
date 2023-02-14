f8635a8fb92816bd607062e6733d74973f38d4c5
Hisham Paloli <hishampaloli786@gmail.com>
2023-02-14 13:06:04 +0530

    prisma-expense @models

--- resource/src/app/repository/mysql/prisma/index.ts
+++ resource/src/app/repository/mysql/prisma/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
 import employeeRepository from "./employee.repository";
+import assetRepository from "./asset.repository";
 
-export { employeeRepository };
+export { employeeRepository, assetRepository };
