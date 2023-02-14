ebeb5d07ddf6567a82df16be23bbc62ea66920cc
Hisham Paloli <hishampaloli786@gmail.com>
2023-02-14 13:26:15 +0530

    prisma-expense @repository

--- chat/src/controller/socket/baseController.ts
+++ chat/src/controller/socket/baseController.ts
@@ -2,8 +2,8 @@ import { DepenteniciesData } from "../../entities/interfaces";
 import { Socket } from "socket.io";
 
 export abstract class BaseController {
-  socket: Socket;
-  dependencies: DepenteniciesData;
+  protected socket: Socket;
+  protected dependencies: DepenteniciesData;
 
   constructor(socket: any, dependencies: any) {
     this.socket = socket;
--- resource/src/app/repository/mysql/prisma/expense.repository.ts
+++ resource/src/app/repository/mysql/prisma/expense.repository.ts
@@ -1,92 +1,92 @@
-// import { ExpenseData } from "../../../../entities/Expense";
-// import { prisma } from "./index";
+import { ExpenseData } from "../../../../entities/Expense";
+import { prisma } from "./index";
 
-// export = {
-//   createAsset: async (asset: ExpenseData) => {
-//     const postgresObj = await prisma.employee.create({
-//       data: {
-//         assetName: asset.assetName,
-//         companyName: asset.companyName,
-//         price: asset.price,
-//         createdBy: { connect: { id: asset.createdBy } },
-//       },
-//     });
+export = {
+  createExpense: async (expense: ExpenseData) => {
+    const postgresObj = await prisma.expense.create({
+      data: {
+        expenseName: expense.expenseName,
+        companyName: expense.companyName,
+        price: expense.price,
+        createdBy: { connect: { id: expense.createdBy } },
+      },
+    });
 
-//     return postgresObj;
-//   },
+    return postgresObj;
+  },
 
-//   getAllAssets: async (companyName: string, pageNumber: number) => {
-//     const postgresObj = await prisma.asset.findMany({
-//       where: { companyName },
-//       orderBy: {
-//         createdAt: "desc",
-//       },
-//       include: { createdBy: true },
-//     });
+  getAllExpenses: async (companyName: string, pageNumber: number) => {
+    const postgresObj = await prisma.expense.findMany({
+      where: { companyName },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: { createdBy: true },
+    });
 
-//     return postgresObj;
-//   },
+    return postgresObj;
+  },
 
-//   getMyAssetPosts: async (companyName: string, createdBy: string) => {
-//     const postgresObj = await prisma.asset.findMany({
-//       where: {
-//         AND: [{ companyName }, { employeeId: createdBy }],
-//       },
+  getMyExpenses: async (companyName: string, createdBy: string) => {
+    const postgresObj = await prisma.expense.findMany({
+      where: {
+        AND: [{ companyName }, { employeeId: createdBy }],
+      },
 
-//       include: { createdBy: true },
-//     });
-//     return postgresObj;
-//   },
+      include: { createdBy: true },
+    });
+    return postgresObj;
+  },
 
-//   getSingleAsset: async (companyName: string, assetId: string) => {
-//     const postgresObj = await prisma.asset.findFirst({
-//       where: {
-//         AND: [{ companyName }, { id: assetId }],
-//       },
-//       include: { createdBy: true },
-//     });
+  getSingleExpense: async (companyName: string, expenseId: string) => {
+    const postgresObj = await prisma.expense.findFirst({
+      where: {
+        AND: [{ companyName }, { id: expenseId }],
+      },
+      include: { createdBy: true },
+    });
 
-//     return postgresObj;
-//   },
+    return postgresObj;
+  },
 
-//   editAssets: async (
-//     companyName: string,
-//     assetId: string,
-//     createdBy: string,
-//     data: any
-//   ) => {
-//     const postgresObj = await prisma.asset.updateMany({
-//       where: {
-//         AND: [{ companyName }, { id: assetId }, { employeeId: createdBy }],
-//       },
-//       data: {
-//         price: data.price,
-//         assetName: data.assetName,
-//       },
-//     });
+  editExpense: async (
+    companyName: string,
+    expenseId: string,
+    createdBy: string,
+    data: any
+  ) => {
+    const postgresObj = await prisma.expense.updateMany({
+      where: {
+        AND: [{ companyName }, { id: expenseId }, { employeeId: createdBy }],
+      },
+      data: {
+        price: data.price,
+        expenseName: data.expenseName,
+      },
+    });
 
-//     const updatedItem = await prisma.asset.findFirst({
-//       where: {
-//         AND: [{ companyName }, { id: assetId }, { employeeId: createdBy }],
-//       },
-//     });
-//     return updatedItem;
-//   },
+    const updatedItem = await prisma.expense.findFirst({
+      where: {
+        AND: [{ companyName }, { id: expenseId }, { employeeId: createdBy }],
+      },
+    });
+    return updatedItem;
+  },
 
-//   deleteAsset: async (
-//     companyName: string,
-//     assetId: string,
-//     createdBy: string
-//   ) => {
-//     const postgresObj = await prisma.asset.deleteMany({
-//       where: {
-//         AND: [
-//           { companyName },
-//           { id: assetId },
-//           { employeeId: { equals: createdBy } },
-//         ],
-//       },
-//     });
-//     return postgresObj;
-//   },
-// };
+  deleteExpense: async (
+    companyName: string,
+    expenseId: string,
+    createdBy: string
+  ) => {
+    const postgresObj = await prisma.expense.deleteMany({
+      where: {
+        AND: [
+          { companyName },
+          { id: expenseId },
+          { employeeId: { equals: createdBy } },
+        ],
+      },
+    });
+    return postgresObj;
+  },
+};
