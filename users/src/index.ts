bfaf90f1b4f370f790458e35dd5078470dc5afaf
Hisham Paloli <hishampaloli786@gmail.com>
2023-02-19 10:16:06 +0530

    test

--- users/src/index.ts
+++ users/src/index.ts
@@ -4,7 +4,7 @@ import { evnCheckers } from "./config/env-checker";
 import { connectNats } from "./config/message-broker";
 
 const start = async () => {
-  console.log('staring 786');
+  console.log('staring 786...');
   
   try {
     evnCheckers();
